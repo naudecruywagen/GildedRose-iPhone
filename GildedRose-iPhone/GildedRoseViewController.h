281cab4f2123395b7cc9620fa2eda97d950de23a
Naudé Cruywagen <naude@glucode.com>
2016-07-10 20:18:57 +0200

    Added GildedRose logic to GildedRoseViewController

--- GildedRose-iPhone/GildedRoseViewController.h
+++ GildedRose-iPhone/GildedRoseViewController.h
@@ -1,7 +1,12 @@
 #import <UIKit/UIKit.h>
 
+@class Item;
+
 @interface GildedRoseViewController : UITableViewController
 
+@property (nonatomic, strong) NSArray<Item *> *items;
+
+- (instancetype)initWith:(NSArray<Item *> *)items;
 
 @end
 
--- GildedRose-iPhone/GildedRoseViewController.m
+++ GildedRose-iPhone/GildedRoseViewController.m
@@ -1,9 +1,82 @@
 #import "GildedRoseViewController.h"
-
-@interface GildedRoseViewController ()
-
-@end
+#import "Item.h"
 
 @implementation GildedRoseViewController
+
+- (instancetype)initWith:(NSArray<Item *> *)items {
+    self = [self initWithStyle:UITableViewStylePlain];
+    if (self) {
+        _items = items;
+    }
+    return self;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.items.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ItemCellIdentifier"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1
+                                      reuseIdentifier:@"ItemCellIdentifier"];
+    }
+    cell.textLabel.text = [self.items[(NSUInteger) indexPath.row] name];
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"Quality: %lu, Sell in: %lu",
+                                                           (unsigned long) [self.items[(NSUInteger) indexPath.row] quality],
+                                                           (unsigned long) [self.items[(NSUInteger) indexPath.row] sellIn]];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSUInteger row = (NSUInteger) indexPath.row;
+    if (![[self.items[row] name] isEqualToString:@"Aged Brie"] && ![[self.items[row] name] isEqualToString:@"Backstage passes to a TAFKAL80ETC concert"]) {
+        if ([self.items[row] quality] > 0) {
+            if (![[self.items[row] name] isEqualToString:@"Sulfuras, Hand of Ragnaros"]) {
+                [self.items[row] setQuality:[self.items[row] quality] - 1];
+            }
+        }
+    }
+    else {
+        if ([self.items[row] quality] < 50) {
+            [self.items[row] setQuality:[self.items[row] quality] + 1];
+            if ([[self.items[row] name] isEqualToString:@"Backstage passes to a TAFKAL80ETC concert"]) {
+                if ([self.items[row] sellIn] < 11) {
+                    if ([self.items[row] quality] < 50) {
+                        [self.items[row] setQuality:[self.items[row] quality] + 1];
+                    }
+                }
+                if ([self.items[row] sellIn] < 6) {
+                    if ([self.items[row] quality] < 50) {
+                        [self.items[row] setQuality:[self.items[row] quality] + 1];
+                    }
+                }
+            }
+        }
+    }
+    if (![[self.items[row] name] isEqualToString:@"Sulfuras, Hand of Ragnaros"]) {
+        [self.items[row] setSellIn:[self.items[row] sellIn] - 1];
+    }
+    if ([self.items[row] sellIn] < 0) {
+        if (![[self.items[row] name] isEqualToString:@"Aged Brie"]) {
+            if (![[self.items[row] name] isEqualToString:@"Backstage passes to a TAFKAL80ETC concert"]) {
+                if ([self.items[row] quality] > 0) {
+                    if (![[self.items[row] name] isEqualToString:@"Sulfuras, Hand of Ragnaros"]) {
+                        [self.items[row] setQuality:[self.items[row] quality] - 1];
+                    }
+                }
+            }
+            else {
+                [self.items[row] setQuality:[self.items[row] quality] - [self.items[row] quality]];
+            }
+        }
+        else {
+            if ([self.items[row] quality] < 50) {
+                [self.items[row] setQuality:[self.items[row] quality] + 1];
+            }
+        }
+    }
+}
+
 
 @end
